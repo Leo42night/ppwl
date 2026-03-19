526d6791c128e4d0a646aca5b4feed3867b38699
Leo42night <karmaborutovvo@gmail.com>
2026-03-19 14:52:12 +0700

    test perubahan .ts

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,13 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // `VITE_` prefix.
-  const env = loadEnv(mode, process.cwd(), '')
-
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
